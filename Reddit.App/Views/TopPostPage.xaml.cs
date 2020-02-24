5a199a732bcd4dc3a9b34ce46e97ef6b12444a87
Gabriel Almiñana <r470n@yahoo.com.ar>
2020-02-24 17:19:34 -0300

    Adding the view

---
+++ Reddit.App/Views/TopPostPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Reddit.App.ViewModels;
+using Reddit.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Reddit.App.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class TopPostPage : Page
+    {
+        public TopPostViewModel ViewModel { get; } = new TopPostViewModel();
+
+        public TopPostPage()
+        {
+            this.InitializeComponent();
+            Loaded += TopPostPage_Loaded;
+        }
+
+        private async void TopPostPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            ProgressRing.IsActive = true;
+            ProgressRing.Visibility = Visibility.Visible;
+            await ViewModel.LoadDataAsync();
+            ProgressRing.IsActive = false;
+            ProgressRing.Visibility = Visibility.Collapsed;
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            ViewModel.Selected = (Post)e.ClickedItem;
+        }
+    }
+}
