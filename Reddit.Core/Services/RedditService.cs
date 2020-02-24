43b7a1f26a333d47df62bcb808f965d5cc537a4e
Gabriel Almiñana <r470n@yahoo.com.ar>
2020-02-24 14:58:28 -0300

    Add reddit service with restsharp

---
+++ Reddit.Core/Services/RedditService.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Reddit.Core.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reddit.Core.Services
+{
+    public static class RedditService
+    {
+        public static async Task<RedditDTO> GetTopPost()
+        {
+            var client = new RestClient("https://www.reddit.com/r");
+            var request = new RestRequest("csharp/top.json", DataFormat.Json);
+            var response = client.Get(request);
+            var result = JsonConvert.DeserializeObject<RedditDTO>(response.Content);
+
+            return result;
+        }
+    }
+}
